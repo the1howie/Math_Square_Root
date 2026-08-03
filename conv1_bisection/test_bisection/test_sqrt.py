10c71c26d14b9875c97fbdc3633b0d7f1652a1fe
Howie Nicolae <the1howie@gmail.com>
2026-08-03 22:32:07 +0100

    Add more tests for bisection search

--- conv1_bisection/test_bisection/test_sqrt.py
+++ conv1_bisection/test_bisection/test_sqrt.py
@@ -22,9 +22,21 @@ def run_sqrt_with_print(S):
 @pytest.mark.parametrize(
     "value, expected, printout",
     [
+        (1, 0.9999999999995453, "Number of guesses: 40, Threshold: 1e-12.\n"),
         (2, 1.4142135623733338, "Number of guesses: 38, Threshold: 1e-12.\n"),
+        (3, 1.732050807568612, "Number of guesses: 40, Threshold: 1e-12.\n"),
+        (4, 2.0, "Number of guesses: 0, Threshold: 1e-12.\n"),
+        (9, 3.0000000000000853, "Number of guesses: 44, Threshold: 1e-12.\n"),
         (10, 3.162277660168229, "Number of guesses: 42, Threshold: 1e-12.\n"),
         (25, 5.000000000000071, "Number of guesses: 45, Threshold: 1e-12.\n"),
+        (37, 6.082762530298236, "Number of guesses: 47, Threshold: 1e-12.\n"),
+        (49, 6.999999999999975, "Number of guesses: 47, Threshold: 1e-12.\n"),
+        (73, 8.54400374531758, "Number of guesses: 48, Threshold: 1e-12.\n"),
+        (81, 8.999999999999968, "Number of guesses: 47, Threshold: 1e-12.\n"),
+        (100, 9.999999999999964, "Number of guesses: 48, Threshold: 1e-12.\n"),
+        (121, 10.99999999999999, "Number of guesses: 49, Threshold: 1e-12.\n"),
+        (1000, 31.62277660168378, "Number of guesses: 52, Threshold: 1e-12.\n"),
+        (10000, 100.0, "Number of guesses: 58, Threshold: 1e-12.\n"),
     ],
 )
 def test_sqrt(value, expected, printout):
